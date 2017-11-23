4ca857a96b8c6b8e1538bdca03639be0d5622504
unknown <florian-hagens@hotmail.com>
2017-11-23 12:11:02 +0000

    highscores fuckd

--- src/views/Highscores.cpp
+++ src/views/Highscores.cpp
@@ -4,23 +4,18 @@ void Highscores::build() {
   lcd.fillScreen(background_color);
   lcd.write("Highscores", 75, screen_height - 230, 2);
 
-  this->printScores();
-
-   Image image;
-   Image gold;
-
    int left = 50;
    int right = 250;
+   this->printScores();
 
-   
+  Image image;
+
    image.build("Gold.bmp", left, 40); //left
    image.build("Gold.bmp", right, 40);//right
    image.build("Silver.bmp",left, 100);//left
    image.build("Silver.bmp",right, 100);//right
    image.build("Bronze.bmp",left, 160);//left
-   image.build("Bronze.bmp",5, 5);//right
-   gold.build("Gold.bmp",5, 5);//right
-
+   image.build("Bronze.bmp",right, 160);//right
 }
 
 /**
@@ -30,11 +25,6 @@ void Highscores::build() {
  */
 void Highscores::printScores()
 {
-  if (! SD.begin(4))
-  {
-    lcd.write(5, 5, "failed", 1);
-  }
-
   File scores = SD.open("scores.txt");
 
   int i    = 0;
@@ -64,7 +54,7 @@ void Highscores::printScores()
 
   buffer[i] = '\0';
   lcd.write(buffer, 100, plek * 60);
-
+  scores.flush();
   scores.close();
 }
 
