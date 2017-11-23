7bc7322e3d41a72e692f2b918604f8a47664cf5e
unknown <florian-hagens@hotmail.com>
2017-11-23 10:42:38 +0000

    pictures added, not working, waiting on gijsje

--- src/support/CharacterSelect.cpp
+++ src/support/CharacterSelect.cpp
@@ -2,6 +2,13 @@
 
 void CharacterSelect::build() {
     lcd.fillScreen(background_color);
-    lcd.write("Select Character", 75, screen_height - 230, 2);
+    lcd.write("Select character", 30, screen_height - 230, 2);
+
+    Image image;
+
+    image.build("LuiKang.bmp", 50, 50);         
+    image.build("Scorpion.bmp", 100, 50);            
+    image.build("Sonya.bmp", 150, 50);           
+    image.build("SubZero.bmp", 200, 50);
 
 }
