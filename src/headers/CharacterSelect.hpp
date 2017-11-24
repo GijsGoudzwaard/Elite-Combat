fc8b2ec5aa91077cc0f18e3c8ffc82412dacaa6d
unknown <florian-hagens@hotmail.com>
2017-11-24 11:09:46 +0000

    charachter select screen almost working

--- src/headers/CharacterSelect.hpp
+++ src/headers/CharacterSelect.hpp
@@ -9,6 +9,32 @@ class CharacterSelect
   public:
     void build();
 
+    uint8_t clickedCharacter(uint_least16_t x, uint_least16_t y);
+
+    void setTouchListener();
+
+    void setCharacter(uint8_t character);
+
+
+  private:
+
+    int buttons[4][4] = {
+      {
+        (screen_width / 2) - 85,
+        90,
+        (screen_width / 2) - 85 + 180,
+        90 + 30,
+      },
+      {
+          95,40,59,74
+      },
+      {
+          165,40,59,74
+      },
+      {
+          235,40,59,74
+      },
+    };
 };
 
 #endif
--- src/views/CharacterSelect.cpp
+++ src/views/CharacterSelect.cpp
@@ -8,35 +8,85 @@ void CharacterSelect::build() {
     int agility = 1;
     int strength = 3;
     
-
-    Image image;
+    // Image image;
     
-    image.build("LiuKang/LiuKang.bmp", 25, 40);         
-    image.build("Scorpion/Scorpion.bmp", 95, 40);            
-    image.build("Sonya/Sonya.bmp", 165, 40);           
-    image.build("SubZero/SubZero.bmp", 235, 40);
+    // image.build("LiuKang/LiuKang.bmp", 25, 40);         
+    // image.build("Scorpion/Scorpion.bmp", 95, 40);            
+    // image.build("Sonya/Sonya.bmp", 165, 40);           
+    // image.build("SubZero/SubZero.bmp", 235, 40);
 
-    lcd.write("Defence", 100, 150);
-    lcd.write("Agility", 100, 175);
-    lcd.write("Strength", 100, 200);
+    // lcd.write("Defence", 100, 150);
+    // lcd.write("Agility", 100, 175);
+    // lcd.write("Strength", 100, 200);
 
-    //change on selected character
-    lcd.write("Scorpion", 25, 130);
-    image.build("Scorpion/selected.bmp", 30, 145);
+    // //change on selected character
+    // lcd.write("Scorpion", 25, 130);
+    // image.build("Scorpion/selected.bmp", 30, 145);
+
+    this->setTouchListener();
+
+}
+
+            //coordinates
 
 
+    uint8_t CharacterSelect::clickedCharacter(uint_least16_t x, uint_least16_t y)
+    {
+      int i;
+      for (i = 0; i < 3; i++) {
+        if (((this->buttons[i][0] <= x) && (x <= this->buttons[i][2])) &&
+            (this->buttons[i][1] <= y) && (y <= this->buttons[i][3])) {
+          return i + 1;
+        }
+      }
+    
+      return 0;
+    }
+
+    void CharacterSelect::setTouchListener()
+    {
+        while (lcd.getActivePage() == 1) {
+        if (lcd.touchRead()) {
+          uint8_t character = this->clickedCharacter(lcd.touchX(), lcd.touchY());
+          // Wait until release the touchscreen.
+          while (lcd.touchRead()) {}
+          this->setCharacter(character);
+        }
+        
+      }
+    }
+
+
+      void CharacterSelect::setCharacter(uint8_t character)
+      {
+        // Use an if else because a switch case was buggy.
+        if (character == 1) {
+            lcd.drawRect(25,40,59,74, RGB(255,255,255));
+        } else if (character == 2) {
+            lcd.drawRect(95,40,59,74, RGB(255,255,255));
+        } else if (character == 3) {
+            lcd.drawRect(165,40,59,74, RGB(255,255,255));
+        } else if (character == 4) {
+            lcd.drawRect(235,40,59,74, RGB(255,255,255));
+        }
+      }
+
+      //Coordinates of the characters
+    //   lcd.drawRect(25,40,59,74, RGB(255,255,255));
+    //   lcd.drawRect(95,40,59,74, RGB(255,255,255));
+    //   lcd.drawRect(165,40,59,74, RGB(255,255,255));
+    //   lcd.drawRect(235,40,59,74, RGB(255,255,255));
 
     //printing stats
-    int i;
-    for(i=0; i<defence; i++){
-        image.build("star.bmp", 170 + i*20, 140);
-    }
-    for(i=0; i<agility; i++){
-        image.build("star.bmp", 170 + i*20, 165);
-    }
-    for(i=0; i<strength; i++){
-        image.build("star.bmp", 170 + i*20, 190);
-    }
+    // int i;
+    // for(i=0; i<defence; i++){
+    //     image.build("star.bmp", 170 + i*20, 140);
+    // }
+    // for(i=0; i<agility; i++){
+    //     image.build("star.bmp", 170 + i*20, 165);
+    // }
+    // for(i=0; i<strength; i++){
+    //     image.build("star.bmp", 170 + i*20, 190);
+    // }
 
-    
-}
+
