f95209dc4fe2e29e99395a53efe934236ac8e1f1
Arjan Kleefman <arjan.kleefman@windesheim.nl>
2017-11-30 13:07:40 +0000

    Added comments on the character classes

--- src/characters/Sonya.cpp
+++ src/characters/Sonya.cpp
@@ -1,5 +1,14 @@
 #include "../headers/Sonya.hpp"
-
+/**
+ * Setting the statistics for the character and linking the correct bmp files with the character
+ *
+ * @param  char *stand
+ * @param  char *hit
+ * @param  char *kick
+ * @param  char *block
+ * @param  char *duck
+ * @return void
+ */
 Sonya::Sonya()
 {
   this->agility = 2;
--- src/characters/SubZero.cpp
+++ src/characters/SubZero.cpp
@@ -1,5 +1,14 @@
 #include "../headers/Subzero.hpp"
-
+/**
+ * Setting the statistics for the character and linking the correct bmp files with the character
+ *
+ * @param  char *stand
+ * @param  char *hit
+ * @param  char *kick
+ * @param  char *block
+ * @param  char *duck
+ * @return void
+ */
 Subzero::Subzero()
 {
   this->agility = 1;
