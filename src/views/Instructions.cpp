c9a30d22e3dd63d1c7b304f7922fd7eeeb89c09d
Gijs Goudzwaard <gijs.goudzwaard@gmail.com>
2017-11-30 13:50:55 +0100

    Added some comments to the game instructions page

--- src/views/Instructions.cpp
+++ src/views/Instructions.cpp
@@ -1,5 +1,10 @@
 #include "../headers/Instructions.hpp"
 
+/**
+ * Build the UI of the game instructions page.
+ *
+ * @return void
+ */
 void Instructions::build()
 {
   Image image;
@@ -7,15 +12,15 @@ void Instructions::build()
   lcd.fillScreen(background_color);
   lcd.write("Game instructions", 20, screen_height - 230, 2);
 
-  image.build("cdown.bmp", 60, 135);         // downside
-  image.build("cup.bmp", 60, 70);            // upside
-  image.build("hit.bmp", 185, 60);           // c
-  image.build("kick.bmp", 185, 170);         // z
+  image.build("cdown.bmp", 60, 135); // downside
+  image.build("cup.bmp", 60, 70);    // upside
+  image.build("hit.bmp", 185, 60);   // c
+  image.build("kick.bmp", 185, 170); // z
 
-  lcd.write("Block", 100, 65);  // up
-  lcd.write("Walk", 25, 140); // left
-  lcd.write("Walk", 190, 140); // right
-  lcd.write("Duck", 105, 220); // down
-  lcd.write("Hit", 230, 75);  // c
-  lcd.write("Kick", 240, 195); // z
+  lcd.write("Block", 100, 65);       // up
+  lcd.write("Walk", 25, 140);        // left
+  lcd.write("Walk", 190, 140);       // right
+  lcd.write("Duck", 105, 220);       // down
+  lcd.write("Hit", 230, 75);         // c
+  lcd.write("Kick", 240, 195);       // z
 }
