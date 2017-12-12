e5cb038ab069a640d93fa46b0ea3833fa0e9773f
Gijs Goudzwaard <gijs.goudzwaard@gmail.com>
2017-12-12 16:16:09 +0100

    Subzero images are now also mirrored

--- src/characters/SubZero.cpp
+++ src/characters/SubZero.cpp
@@ -17,17 +17,17 @@ Subzero::Subzero()
   this->defence = 3;
   this->strength = 2;
 
-//  if (this->isRightPlayer()) {
-//    this->stand_stance = (__FlashStringHelper *) F("SuStandM.bmp");
-//    this->hit_stance = (__FlashStringHelper *) F("SuHitM.bmp");
-//    this->kick_stance = (__FlashStringHelper *) F("SuKickM.bmp");
-//    this->block_stance = (__FlashStringHelper *) F("SuBlockM.bmp");
-//    this->duck_stance = (__FlashStringHelper *) F("SuDuckM.bmp");
-//  } else {
+  if (this->isRightPlayer()) {
+    this->stand_stance = (__FlashStringHelper *) F("SuStandM.bmp");
+    this->hit_stance = (__FlashStringHelper *) F("SuHitM.bmp");
+    this->kick_stance = (__FlashStringHelper *) F("SuKickM.bmp");
+    this->block_stance = (__FlashStringHelper *) F("SuBlockM.bmp");
+    this->duck_stance = (__FlashStringHelper *) F("SuDuckM.bmp");
+  } else {
     this->stand_stance = (__FlashStringHelper *) F("SuStand.bmp");
     this->hit_stance = (__FlashStringHelper *) F("SuHit.bmp");
     this->kick_stance = (__FlashStringHelper *) F("SuKick.bmp");
     this->block_stance = (__FlashStringHelper *) F("SuBlock.bmp");
     this->duck_stance = (__FlashStringHelper *) F("SuDuck.bmp");
-//  }
+  }
 }
